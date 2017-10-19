5764c6b8a8c6f923e51311224ba6e4168cebd06e
michael.boutboul87 <michael.boutboul87@gmail.com>
2017-10-19 03:33:23 -0400

    bug & tests corrected

--- app/models/town.rb
+++ app/models/town.rb
@@ -1,10 +1,10 @@
 class Town < ActiveRecord::Base
-  before_validation: load_position
+  before_validation :load_position
   
   private
   def load_position
     places = Nominatim.search(name).limit(1)
-    latitude = places.first.lat
-    longitude = places.first.long
+    self.latitude = places.first.lat
+    self.longitude = places.first.lon
+  end
 end
-  
